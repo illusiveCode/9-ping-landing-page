3408597e563930ed185f77777b4e4ae9bee4081e
Muhammad Raza <mh.raza2906@gmail.com>
2024-08-05 21:55:08 +0100

    feat: write markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,39 @@
 const tailwindConfig = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: {
-        blue: "#4C7BF3",
-        navy: "15202A",
+    extend: {
+      colors: {
+        primary: {
+          blue: "#4C7BF3",
+          navy: "15202A",
+        },
+        neutral: {
+          grey: "#969696",
+          smurf: "#C2D3FF",
+          "light-blue": "#B8C7ED",
+          white: "#FFFFFF",
+        },
       },
-      neutral: {
-        grey: "#969696",
-        smurf: "#C2D3FF",
-        "light-blue": "#B8C7ED",
-        white: "#FFFFFF",
+      screens: {
+        mobile: "375px",
+        tablet: "768px",
+        laptop: "1024px",
+        desktop: "1200px",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1.2rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+      fontFamily: {
+        primary: '"Libre Franklin", sans-serif',
       },
     },
-    screens: {
-      mobile: "375px",
-      tablet: "768px",
-      laptop: "1024px",
-      desktop: "1200px",
-    },
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "1.2rem",
-        sm: "2rem",
-        lg: "4rem",
-        xl: "5rem",
-        "2xl": "6rem",
-      },
-    },
-    fontFamily: {
-      primary: '"Libre Franklin", sans-serif',
-    },
-    extend: {},
   },
   plugins: [],
 };
